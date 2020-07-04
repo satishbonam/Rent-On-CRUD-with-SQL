24a750cf155dbe8f8391ac2689d72dba1e09342f
Rahul7744 <satish.b.s.kumar@gmail.com>
2020-07-04 16:20:26 +0530

    Added User Model

---
+++ src/server/app/main/models/UserModel.py
@@ -0,0 +1,13 @@
+from .. import db
+
+
+class UserModel(db.Model):
+
+    __tablename__ = "user"
+
+    id = db.Column(db.Interger, primary_key=True)
+    name = db.Column(db.String(70), nullable=False)
+    email = db.Column(db.String(70), nullable=False)
+    mobile = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(70), nullable=False)
