f2d602de2aed97949cf22c30642aa1982a7da2a0
Rahul7744 <satish.b.s.kumar@gmail.com>
2020-07-04 16:18:57 +0530

    Updated settings.py

--- src/server/app/main/settings.py
+++ src/server/app/main/settings.py
@@ -5,13 +5,15 @@ import os
 # postgres_local_base = os.environ['DATABASE_URL']
 
 #env = Env()
-#env.read_env()
+# env.read_env()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
     DEBUG = False
+
 
 class DevelopmentConfig(Config):
     # uncomment the line below to use postgres
@@ -20,22 +22,25 @@ class DevelopmentConfig(Config):
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://<user>:<password>@<host>:<port>/<database_name>'
 
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:<password>@localhost/<database>'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:satish@localhost/rent_crud'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ENV = 'development'
+
 
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:<password>@localhost/<database>'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:satish@localhost/rent_crud'
     PRESERVE_CONTEXT_ON_EXCEPTION = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class ProductionConfig(Config):
     DEBUG = False
 
     # uncomment the line below to use postgres
     # SQLALCHEMY_DATABASE_URI = postgres_local_base
+
 
 config_by_name = dict(
     dev=DevelopmentConfig,
