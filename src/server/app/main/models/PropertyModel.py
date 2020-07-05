7438fe81edd8f7689a2262c840531ab4d209f6d5
Rahul7744 <satish.b.s.kumar@gmail.com>
2020-07-05 17:38:39 +0530

    Updated Property model

--- src/server/app/main/models/PropertyModel.py
+++ src/server/app/main/models/PropertyModel.py
@@ -5,10 +5,10 @@ class PropertyModel(db.Model):
 
     __tablename__ = "property"
 
-    id = db.Column(db.Interger, primary_key=True)
-    owner_id = db.Column(db.Interger, db.ForeignKey('user.id'), nullable=False)
-    area = db.Column(db.Interger, nullable=False)
-    bedrooms = db.Column(db.Interger, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    area = db.Column(db.Integer, nullable=False)
+    bedrooms = db.Column(db.Integer, nullable=False)
     furnishing = db.Column(db.Boolean, nullable=False, default=False)
     locality = db.Column(db.String(255), nullable=False)
     isRented = db.Column(db.Boolean, nullable=False, default=False)
