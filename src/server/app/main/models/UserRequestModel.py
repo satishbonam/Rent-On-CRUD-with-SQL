dfd35f755dc88cb5f2f0db430b2b1070ce79c46e
Rahul7744 <satish.b.s.kumar@gmail.com>
2020-07-05 17:39:38 +0530

    Added user request modal

--- src/server/app/main/models/UserRequestModel.py
+++ src/server/app/main/models/UserRequestModel.py
@@ -5,9 +5,9 @@ class UserRequestModel(db.Model):
 
     __tablename__ = "user_request"
 
-    id = db.Column(db.Interger, primary_key=True)
-    user_id = db.Column(db.Interger, db.ForeignKey(
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(
         'user.id'), nullable=False)
-    owner_id = db.Column(db.Interger, db.ForeignKey(
+    owner_id = db.Column(db.Integer, db.ForeignKey(
         'user.id'), nullable=False)
     message = db.Column(db.String(255), nullable=True)
